6b59452babe962084c29556264790dc0b03a027f
Free <2331141557@qq.com>
2023-02-25 20:57:29 +0800

    项目搭建完成，配置完成

---
+++ src/main/java/com/access/AccessDevApplication.java
@@ -0,0 +1,13 @@
+package com.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessDevApplication.class, args);
+    }
+
+}
