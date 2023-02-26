1a6678bf4aacd43df2216fabafbe817b1e25e229
Free <2331141557@qq.com>
2023-02-26 09:40:53 +0800

    实现redis的序列化

---
+++ src/main/java/com/access/config/MyRedisConfig.java
@@ -0,0 +1,56 @@
+package com.access.config;
+
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+@Configuration
+public class MyRedisConfig {
+
+    @Resource
+    private RedisConnectionFactory factory;
+
+    @Bean
+    public RedisTemplate redisTemplate(){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        // 针对键做String序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // 针对值做JSON序列化
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        redisTemplate.setValueSerializer(serializer);
+
+        // 对象映射 对时间、时区进行设置
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        om.setTimeZone(TimeZone.getDefault());
+        om.configure(MapperFeature.USE_ANNOTATIONS, false);
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        serializer.setObjectMapper(om);
+
+        return redisTemplate;
+    }
+}
