ba9ad4688623b85d5556d8dab2cac06a97e86e4d
Free <2331141557@qq.com>
2023-02-26 09:07:26 +0800

    创建公共响应类成功和失败方法

--- src/main/java/com/access/common/vo/ResponseEntity.java
+++ src/main/java/com/access/common/vo/ResponseEntity.java
@@ -14,4 +14,36 @@ public class ResponseEntity<T> {
     private String message;
 
     private T data;
+
+    public static <T> ResponseEntity<T> success(){
+        return new ResponseEntity<>(20000,"success",null);
+    }
+
+    public static <T> ResponseEntity<T> success(T data){
+        return new ResponseEntity<>(20000,"success",data);
+    }
+
+    public static <T> ResponseEntity<T> success(T data,String message){
+        return new ResponseEntity<>(20000,message,data);
+    }
+
+    public static <T> ResponseEntity<T> success(String message){
+        return new ResponseEntity<>(20000,message,null);
+    }
+
+    public static<T>  ResponseEntity<T> fail(){
+        return new ResponseEntity<>(20001,"fail",null);
+    }
+
+    public static<T>  ResponseEntity<T> fail(Integer code){
+        return new ResponseEntity<>(code,"fail",null);
+    }
+
+    public static<T>  ResponseEntity<T> fail(Integer code, String message){
+        return new ResponseEntity<>(code,message,null);
+    }
+
+    public static<T>  ResponseEntity<T> fail( String message){
+        return new ResponseEntity<>(20001,message,null);
+    }
 }
