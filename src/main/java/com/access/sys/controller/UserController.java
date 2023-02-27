3587b7580e37b4e438151b710a6be75a2575bddb
Free <2331141557@qq.com>
2023-02-27 22:00:03 +0800

    实现用户列表查询接口

--- src/main/java/com/access/sys/controller/UserController.java
+++ src/main/java/com/access/sys/controller/UserController.java
@@ -3,10 +3,13 @@ package com.access.sys.controller;
 import com.access.common.vo.ResponseEntity;
 import com.access.sys.entity.User;
 import com.access.sys.service.IUserService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -55,5 +58,25 @@ public class UserController {
     public ResponseEntity<?> loginOut(@RequestHeader("X-token") String token){
         userService.loginOut(token);
         return ResponseEntity.success();
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Map<String,Object>> getUserList(@RequestParam(value = "username",required = false) String username,
+                                                      @RequestParam(value = "phone",required = false) String phone,
+                                                      @RequestParam(value = "pageNo") Long pageNo,
+                                                      @RequestParam(value = "pageSize") Long pageSize){
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper();
+        //条件1
+        wrapper.eq(StringUtils.hasLength(username),User::getUsername,username);
+        //条件2
+        wrapper.eq(StringUtils.hasLength(phone),User::getPhone,phone);
+        Page<User> page = new Page<>(pageNo,pageSize);
+        userService.page(page,wrapper);
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("total",page.getTotal());
+        data.put("rows",page.getRecords());
+
+        return ResponseEntity.success(data);
     }
 }
